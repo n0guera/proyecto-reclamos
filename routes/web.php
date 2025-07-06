291f70ed45bd515c2b6599ea292e17b47fe09a82
agufedee <agufedee@gmail.com>
2025-07-06 20:38:32 -0300

    Correccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
-<?php
-
+<?php 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
+use App\Http\Controllers\ReclamoController;
+use App\Http\Controllers\Auth\LogoutController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', Login::class)->name('login');
+
+Route::get('/register', Register::class)->name('register');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/reclamos/create', [ReclamoController::class, 'create'])->name('reclamos.create');
 });
+
+Route::get('/mi-cuenta', function () {
+    return view('mi-cuenta'); 
+})->name('mi-cuenta');
+
+Route::get('/sobre-nosotros', function () {
+    return view('sobre-nosotros');  
+})->name('sobre-nosotros');
+
+
+Route::post('/logout', LogoutController::class)->name('logout');
+?>
