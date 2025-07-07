fc7b6b8d9e23f76fb95f52c5b1dbdf8ca8a34489
Santiago Noguera <santiiago.noguera@gmail.com>
2025-07-07 00:05:59 -0300

    Agregar componente Bienvenida

---
+++ app/Livewire/Bienvenida.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Bienvenida extends Component
+{
+    public function render()
+    {
+        return view('livewire.bienvenida');
+    }
+}
---
+++ resources/views/livewire/bienvenida.blade.php
@@ -0,0 +1,2 @@
+<div class="bg-white h-150 w-100 rounded-md">
+</div>
