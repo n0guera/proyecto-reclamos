e4f00c7a4e9b18588b80830153db3d2c3cd77517
Santiago Noguera <santiiago.noguera@gmail.com>
2025-07-07 08:57:18 -0300

    Agregar logo a la bienvenida

--- resources/views/livewire/bienvenida.blade.php
+++ resources/views/livewire/bienvenida.blade.php
@@ -1,2 +1,4 @@
-<div class="bg-white h-150 w-100 rounded-md">
+<div class="flex flex-col justify-center bg-white rounded-md p-5">
+    <img src="{{ asset('images/logo_narnia.svg') }}" alt="Logo">
+    <h1 class="font-medium text-2xl">Bienvenido al Registro de Reclamos</h1>
 </div>
