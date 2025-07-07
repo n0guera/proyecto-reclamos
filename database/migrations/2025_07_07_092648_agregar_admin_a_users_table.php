0ea4c0f5faca3cf6f427185ec622ec469b63cf7f
EliasViotti <elias_viotti_95@hotmail.com.ar>
2025-07-07 14:48:30 -0300

    Se agregó la generación de un perfil administrador por defecto en las migraciones

--- database/migrations/2025_07_07_092648_agregar_admin_a_users_table.php
+++ database/migrations/2025_07_07_092648_agregar_admin_a_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -14,6 +16,14 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('is_admin')->default(false);
         });
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'dni' => '12345678',
+            'email' => 'admin@admin.com',
+            'domicilio' => 'asd',
+            'password' => Hash::make('123456'),
+            'is_admin' => '1',
+        ]);
     }
 
     /**
